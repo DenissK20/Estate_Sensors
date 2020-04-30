f6f839035b11dab05c3707544c4a9b5c4461cec6
DenissK20 <deniska.riga@gmail.com>
2020-04-30 11:29:29 +0300

    up

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-require 'vendor/autoload.php';
+require 'models.php';
 
 $app = new \atk4\ui\App('Ielogoties sistēmā');
 $app->initLayout('Centered');
+
+$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=estatesensors;host=localhost','root','');
