3e914566866543924503a5f0ec2a82bddd2670fb
DenissK20 <deniska.riga@gmail.com>
2020-05-01 15:37:35 +0300

    up

--- index.php
+++ index.php
@@ -4,17 +4,16 @@ require 'models.php';
 $app = new \atk4\ui\App('Ielogoties sistēmā');
 $app->initLayout('Centered');
 
-$db = new
-\atk4\data\Persistence_SQL('mysql:dbname=estatesensors;host=localhost','root','');
 
-$user_login = new User_login($db);
+
+$user_login = new Users_login($db);
   $form = $app->layout->add('Form');
-$form->setModel(new User_login($db),['login','password']);
+$form->setModel(new Users_login($db),['login','password']);
 $form->buttonSave->set('Sign in');
-$form->onSubmit(function($form) use ($user_login) {
-  $user_login->tryLoadBy('login',$form->model['login']);
-  If ($user_login['password'] == $form->model['password']) {
-    $_SESSION['user_login_id'] = $user_login->id;
+$form->onSubmit(function($form)  {
+  $users_login->tryLoadBy('login',$form->model['login']);
+  If ($users_login['password'] == $form->model['password']) {
+    $_SESSION['user_login_id'] = $users_login->id;
     return new \atk4\ui\jsExpression('document.location="main.php"');
   } else {
     $person->unload();
--- models.php
+++ models.php
@@ -2,3 +2,29 @@
 require 'vendor/autoload.php';
 
 session_start();
+
+$db = new
+\atk4\data\Persistence_SQL('mysql:dbname=estatesensors;host=localhost','root','');
+
+class Users extends \atk4\data\Model {
+  public $table = 'users';
+function init() {
+  parent::init();
+  $this->addField('first_name');
+  $this->addField('last_name');
+  $this->addField('user_type');
+  $this->hasOne('Users_login_id', new Users_login);
+
+}
+}
+
+class Users_login extends \atk4\data\Model {
+  public $table = 'users_login';
+function init() {
+  parent::init();
+  $this->addField('login');
+  $this->addField('password',['type'=>'password']);
+  $this->addField('is_active');
+
+}
+}
